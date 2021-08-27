832aad18d136f171d0a7e404f03538a005703224
William Ellison <waellison@me.com>
2021-08-26 20:03:44 -0400

    Refinement: Enabled printing of descriptions in the world map.
    It kinda sorta looks like a real game now!

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ https://github.com/tnwae/nuventure
 """
 
 import nuventure
+import textwrap
 
 game_world = nuventure.world.World("./test-worlds/dirtest.json")
 start_node = game_world.nodes["ORIGIN"]
@@ -12,7 +13,14 @@ player = nuventure.actor.Actor(game_world, start_node)
 
 # TODO: The movement logic here will form the basis of the movement verb later on.
 while True:
-    print(player.location.friendly_name)
+    print(player.location.friendly_name + "\n")
+    desclength = "long" if player.location.visitedp is False else "short"
+    description = player.location.describe(desclength)
+    print(*textwrap.wrap(description, width=72), sep="\n")
+
+    # We can assume wolog that the current location has been visited.
+    player.location.visitedp = True
+    print("")
 
     try:
         direction = input("> ")
