bc93ec285bedfa4e9abc104038bc9d4b75af763c
William Ellison <waellison@me.com>
2021-09-04 14:48:27 -0400

    Added a victory condition.

--- nuventure/verb_callbacks.py
+++ nuventure/verb_callbacks.py
@@ -28,6 +28,7 @@ def do_south(actor: NVActor, *_):
 
 def do_look(actor: NVActor, *_):
     actor.location.render(longp=True)
+    return True
 
 
 def do_inspect(actor: NVActor, target: any, _):
@@ -35,28 +36,37 @@ def do_inspect(actor: NVActor, target: any, _):
     target_itm = actor.bound_world.items[target]
     if target_itm in here.items:
         target_itm.render()
+        return True
+    return False
 
 
 def do_take(actor: NVActor, target, _):
     here = actor.location
-    target_itm = actor.bound_world.items[target]
+    target_itm = actor.bound_world.items.get(target, None)
     if target_itm in here.items:
-        target_itm.take(actor)
+        return actor.add_item(target_itm)
+    return False
 
 
 def do_drop(actor: NVActor, target, _):
     try:
         target_itm = actor.inventory[target]
     except KeyError:
-        print(f"{target} not in inventory")
-        return
-    target_itm.drop(actor)
+        return False
+    return actor.drop_item(target_itm)
 
 
 def do_inventory(actor: NVActor, *_):
     print("\nYour Inventory:")
     for item in actor.inventory.values():
         print(item.short_render())
+
+
+def do_arkhtos(*_):
+    print("You have won!  Please visit https://python.org to collect your prize.\n")
+    print("Spoiler: Your prize is a free download, no strings attached, of the")
+    print("Python programming language, which was used to implement this game.")
+    do_quit()
 
 
 def do_quit(*_):
