1b0445ac1b0bd66f0fb5db68fecb1b66f1dde8da
William Ellison <waellison@me.com>
2021-09-06 11:13:46 -0400

    Stylistic tweaks per pylint.

--- nuventure/__init__.py
+++ nuventure/__init__.py
@@ -1,7 +1,18 @@
+"""Nuventure: A poor man's ScummVM, minus the Z-code.
+
+https://github.com/tnwae/nuventure
+
+Copyright (c) 2021 by William Ellison.
+<waellison@gmail.com>
+
+Nuventure is licensed under the terms of the MIT License, furnished
+in the LICENSE file at the root directory of this distribution.
+"""
+
 import sys
 
 
-directions = {"east", "down", "up", "north", "west", "south"}
+DIRECTIONS = {"east", "down", "up", "north", "west", "south"}
 
 if __name__ == "__main__":
     print("this script is not runnable separately", file=sys.stderr)
