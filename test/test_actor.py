5dca3a2a24729268d688ae50764366d99e05eb1e
William Ellison <waellison@gmail.com>
2022-05-16 19:23:35 -0400

    Completed test coverage of NVActor

--- test/test_actor.py
+++ test/test_actor.py
@@ -53,12 +53,22 @@ def test_npc_detect():
 
 
 def test_player_detect():
-    test_player = NVActor(None, None)
-    assert not test_player.is_npc()
+    actor_fixture.internal_name = "PLAYER"
+    assert not actor_fixture.is_npc()
 
 
-def test_player_move_ok():
+def test_player_move_ok(capsys):
     result = actor_fixture.move("up")
+    captured = capsys.readouterr()
+    assert captured.out == """You shimmy up the ladder to your attic.
+
+Attic
+You look around the attic of your house.  There isn't much left here,
+just a few empty crates that used to store fabric and food.  The air is
+musty and warm, and you start to sweat.
+
+There is a sword lying here in its scabbard.
+"""
     assert result
 
 
@@ -66,12 +76,49 @@ def test_player_move_fail():
     with pytest.raises(NVBadArgError) as ex:
         result = actor_fixture.move("north")
 
-
 def test_item_bestowal():
     result = actor_fixture.add_item(item_fixture)
     assert result
 
 
+def test_item_bestowal_bad_item_fails():
+    result = actor_fixture.add_item(None)
+    assert result is False
+
+
 def test_item_removal():
     result = actor_fixture.drop_item(item_fixture)
     assert result
+
+
+def test_item_removal_bad_item_fails():
+    result = actor_fixture.drop_item(None)
+    assert result is False
+
+
+def test_actor_tic_player_died(capsys):
+    test_actor = NVActor(None, None)
+    test_actor.injure(100)
+
+    with pytest.raises(SystemExit) as exit_exc:
+      test_actor.do_tic()
+      captured = capsys.readouterr()
+      assert captured.out == "You have died."
+    
+    assert exit_exc.type == SystemExit
+
+
+def test_actor_tic_npc_died():
+    test_actor = NVActor(game_fixture.world, game_fixture.start_node, iname="TEST")
+    test_actor.injure(100)
+    test_actor.do_tic()
+    assert test_actor not in game_fixture.world.actors.values()
+
+
+def test_actor_tic_npc_moved():
+    start_node = actor_fixture.location.internal_name
+    actor_fixture.movement_rate = 1
+    actor_fixture.internal_name = "SIDEKICK"
+    actor_fixture.do_tic()
+    assert start_node != actor_fixture.location.internal_name
+
